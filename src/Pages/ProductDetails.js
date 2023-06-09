8ba86a73adb011a0e70d81698f165448b351b95b
Nirvana <shoaibarms@gmail.com>
2023-06-10 01:34:22 +0530

    modified:   src/Pages/ProductDetails.js

--- src/Pages/ProductDetails.js
+++ src/Pages/ProductDetails.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 
 export default function ProductDetails() {
     const { id } = useParams();
     const [product, setProduct] = useState(null);
+    const [del, setDelete] = useState([]);
+    const navigate = useNavigate()
 
     useEffect(() => {
         fetch(`http://localhost:5000/products/${id}`)
@@ -11,16 +13,36 @@ export default function ProductDetails() {
             .then(data => setProduct(data));
     }, [id]);
 
+    const handleDelete = (productId) => {
+        fetch(`http://localhost:5000/products/${productId}`, {
+            method: 'DELETE',
+        })
+            .then(response => response.json())
+            .then(() => {
+                const updatedProducts = del.filter(product => product._id !== productId);
+                setDelete(updatedProducts);
+                navigate('/products')
+            })
+            .catch(error => {
+                console.error('Error deleting product:', error);
+            });
+    };
+
     if (!product) {
         return <div>Loading...</div>;
     }
-
+console.log(product);
     return (
         <div>
         <Link to='/'><h1>Home</h1></Link>
-            <h2>{product.name}</h2>
-            <p>{product.description}</p>
+            <h2>{`Name: ${product.name}`}</h2>
+            <h3>{`Description: ${product.description}`}</h3>
+            <h4>{`Long Description: ${product.longDescription}`}</h4>
             {/* Display other product details */}
+            <Link to={`/products/${id}/edit`}>
+                <button>Edit</button>
+            </Link>
+            <button onClick={() => handleDelete(product._id)}>Delete</button>
         </div>
     );
 }
