8da6cf9240a20e8aff10aeaa17580c69f596b7f4
Nirvana <shoaibarms@gmail.com>
2023-06-09 23:19:48 +0530

    modified:   src/Pages/ProductDetails.js

--- src/Pages/ProductDetails.js
+++ src/Pages/ProductDetails.js
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
-export default function ProductDetails({ match }) {
+export default function ProductDetails() {
+    const { id } = useParams();
     const [product, setProduct] = useState(null);
 
     useEffect(() => {
-        const productId = match.params.id;
-        fetch(`/products/${productId}`)
+        fetch(`http://localhost:5000/products/${id}`)
             .then(response => response.json())
             .then(data => setProduct(data));
-    }, [match.params.id]);
+    }, [id]);
 
     if (!product) {
         return <div>Loading...</div>;
@@ -16,11 +17,10 @@ export default function ProductDetails({ match }) {
 
     return (
         <div>
+        <Link to='/'><h1>Home</h1></Link>
             <h2>{product.name}</h2>
             <p>{product.description}</p>
             {/* Display other product details */}
         </div>
     );
 }
-
-
