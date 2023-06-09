b21fab74bd641c5b09895f7724cc8e38737aba41
Nirvana <shoaibarms@gmail.com>
2023-06-10 01:33:49 +0530

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Pages/Home';
 import Products from './Pages/Products';
 import ProductDetails from './Pages/ProductDetails';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import EditProduct from './Pages/EditProduct';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/products' element={<Products />} />
           <Route path='/products/:id' element={<ProductDetails />} />
+          <Route path='/products/:id/edit' element={<EditProduct />} />
         </Routes>
       </BrowserRouter>
     </>
